fdec86cddbc76f20cda1092ff0bdddfcc3681932
sahil <sahil.98@hotmail.com>
2021-03-06 20:43:12 +0000

    Open a new blank fragment

---
+++ app/src/main/java/com/example/fragmentexercise/FragmentActivity.java
@@ -0,0 +1,15 @@
+package com.example.fragmentexercise;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentActivity extends Fragment {
+
+
+}
