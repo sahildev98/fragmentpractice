46187249d2bbacca8c6088ee8410dc63aaf227f5
sahil <sahil.98@hotmail.com>
2021-03-14 21:52:11 +0000

    Adding fragment container view to MainActivity. Also adding bundle data and adding Textview on FragmentActivity xml. Implement LayoutInflater/ViewGroup.

--- app/src/main/java/com/example/fragmentexercise/FragmentActivity.java
+++ app/src/main/java/com/example/fragmentexercise/FragmentActivity.java
@@ -2,14 +2,43 @@ package com.example.fragmentexercise;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.zip.Inflater;
 
 
 public class FragmentActivity extends Fragment {
 
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
 
-}
+        //        int someInt = requireArguments().getInt("int1");
+//        int someIntTwo = requireArguments().getInt("int2");
+//        int someIntThree = requireArguments().getInt("int3");
+
+        LinearLayout layout = view.findViewById(R.id.fragment_layout);
+
+        ArrayList num = getArguments().getIntegerArrayList("mynumData");
+        Iterator iterator = num.iterator();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_activity, container, false);
+    }
+
+
+
+
+    }
--- app/src/main/java/com/example/fragmentexercise/MainActivity.java
+++ app/src/main/java/com/example/fragmentexercise/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.fragmentexercise;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 
+        FragmentTransaction fragmentTransact = getSupportFragmentManager().beginTransaction();
+        fragmentTransact.add(R.id.fragment_container_view, FragmentActivity.class, insertData());
+        fragmentTransact.commit();
+    }
+
+    public Bundle insertData(){
+        ArrayList <Integer> myNumList = new ArrayList<>();
+        Bundle bundle = new Bundle();
+
+        for (int i = 0; i<5; i++){
+            bundle.putInt("int0", 0);
+            bundle.putInt("int1", 1);
+            bundle.putInt("int2", 2);
+        }
+
+
+        bundle.putIntegerArrayList("mynumData", myNumList);
+        return bundle;
     }
 }
